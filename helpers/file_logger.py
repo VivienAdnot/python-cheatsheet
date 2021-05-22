8646aa268e3429b7493580a1766f24ac43ecaa9f
Vivien Adnot <vivien.adnot@gmail.com>
2021-05-22 05:07:26 +0200

    removed output files

--- helpers/file_logger.py
+++ helpers/file_logger.py
@@ -1,18 +1,18 @@
 # L = ["This is Delhi \n","This is Paris \n","This is London \n"] 
-# file1 = open("myfile.txt","w")
+# file1 = open("myfile.log","w")
 # file1.writelines(L)
 # file1.close()
-# file1 = open("myfile.txt","r+")
+# file1 = open("myfile.log","r+")
 # print("Output of Read function is ")
 # print(file1.read())
 # print()
 # file1.close()
 
-# file1 = open("myfile.txt","w")
+# file1 = open("myfile.log","w")
 # file1.writeline("hello")
 # file1.writeline("toto")
 # file1.close()
 
-with open("myfile.txt","w") as the_file:
+with open("myfile.log","w") as the_file:
   the_file.write("hello\n")
   the_file.write("toto\n")
